5dad74833914eccb08ced42d7895b365626c06b8
Diego5529 <diego.6.souza@gmail.com>
2017-02-05 14:43:34 -0200

    Initial Commit

---
+++ tcc-swift3/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tcc-swift3
+//
+//  Created by Diego Oliveira on 05/02/17.
+//  Copyright © 2017 DO. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
